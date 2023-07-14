61f7be6d3fad8f1c2172f522feb05f1b5b0a5106
cjsolis <cjelizondo12@gmail.com>
2023-07-14 10:46:36 -0500

    fixed styling for about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ import picture from "../components/images/personalPhoto.jpg";
 export default function About() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center px-5 mx:auto mt-10 space-y-0 md:space-y-0">
-      <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+      <div className="container flex flex-col mb-32 space-y-12 md:w-1/2 bg-white bg-opacity-30 rounded-lg backdrop-blur-lg shadow-lg p-8 md:mr-8 text-lg leading-relaxed text-gray-800">
         <p>
           {" "}
           After graduating high school, I embarked on my first career as an
@@ -27,7 +27,7 @@ export default function About() {
           skills, and making a meaningful impact in the tech world.
         </p>
       </div>
-      <img src={picture} alt="personal-photo" className=" max-w-sm  rounded border bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800" />
+      <img src={picture} alt="personal-photo" className=" max-w-sm  rounded border bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800 md:ml-8" />
       <div></div>
     </div>
   );
