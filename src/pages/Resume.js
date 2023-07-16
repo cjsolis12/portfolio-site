ba7bf66c0a6c719b302c7ae81586d8a5cfbb085e
cjsolis <cjelizondo12@gmail.com>
2023-07-16 14:55:43 -0500

    changed resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function Resume() {
     return (
-        <div>
-            <button className='animate-bounce'><a
+        <div className='flex flex-col justify-center h-100vh'>
+            <button className='animate-bounce bg-aqua'><a
           href="https://docs.google.com/document/d/1zsEbW0x16dFTWi2zpNTR1jfq_0-uqW4orMeG2kbzjFs/export?format=pdf"
           download="resume.pdf"
         >
