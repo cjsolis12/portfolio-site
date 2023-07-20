44a5e0c342b41e192bb8748def2f6ee1d6b68d40
cjsolis <cjelizondo12@gmail.com>
2023-07-20 09:53:48 -0500

    modified card style for resume and skills

--- src/components/FrontEndSkills.js
+++ src/components/FrontEndSkills.js
@@ -1,23 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Resume() {
+export default function FrontEndSkills() {
   return (
     <>
       <div className="column bg-white bg-opacity-10 rounded-lg">
-        <div className="row">
-          <button className="text-xl uppercase font-medium mb-3">
-            {" "}
-            Front End{" "}
-          </button>
+        <h2 className="text-xl uppercase font-medium mb-3"> Front End </h2>
 
-          <ul className="flex flex-col justify-center items-center font-thin">
-            <li className="flex items-center space-x-2">React</li>
-            <li className="flex items-center space-x-2">HTML</li>
-            <li className="flex items-center space-x-2">CSS</li>
-            <li className="flex items-center space-x-2">Bootstrap</li>
-            <li className="flex items-center space-x-2">Chakra UI</li>
-          </ul>
-        </div>
+        <ul className="flex flex-col justify-center items-center font-thin">
+          <li className="flex items-center space-x-2">React</li>
+          <li className="flex items-center space-x-2">HTML</li>
+          <li className="flex items-center space-x-2">CSS</li>
+          <li className="flex items-center space-x-2">Bootstrap</li>
+          <li className="flex items-center space-x-2">Chakra UI</li>
+        </ul>
       </div>
     </>
   );
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,45 +1,58 @@
 import React from "react";
 import FrontEndSkills from "../components/FrontEndSkills";
 import BackendSkills from "../components/BackendSkills";
+import OtherSkills from "../components/OtherSkills";
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
-import {FaServer} from "react-icons/fa"
+import { FaServer, FaRegUserCircle } from "react-icons/fa";
 
 export default function Resume() {
   const handleDownload = () => {
-    const fileUrl = "https://docs.google.com/document/d/1zsEbW0x16dFTWi2zpNTR1jfq_0-uqW4orMeG2kbzjFs/export?format=pdf";
-    window.open(fileUrl)
-  }
+    const fileUrl =
+      "https://docs.google.com/document/d/1zsEbW0x16dFTWi2zpNTR1jfq_0-uqW4orMeG2kbzjFs/export?format=pdf";
+    window.open(fileUrl);
+  };
   return (
     <>
       <div className="relative min-h-screen">
-        <div className="flex flex-col justify-center items-center h-screen w-full">
-      <button className="animate-bounce bg-aqua inline-block border rounded-md px-3" onClick={handleDownload}>Download Resume</button>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-          <div className="column bg-white bg-opacity-10 p-4 md:p-8  shadow-lg shadow-grey rounded-lg backdrop-blur-md">
-           <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10 flex justify-center items-center">
-            <span><HiOutlineComputerDesktop/></span>
-           </div>
-            <div className="frontend">
-            <FrontEndSkills />
+        <div className="flex flex-col  justify-center items-center h-screen w-full">
+          <button
+            className="animate-bounce mb-10 bg-aqua inline-block border rounded-md px-3"
+            onClick={handleDownload}
+          >
+            Download Resume
+          </button>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+            <div className="flex flex-col items-center w-60 bg-white bg-opacity-20 p-4 md:p-8  shadow-lg shadow-grey rounded-lg backdrop-blur-md">
+              <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10 flex justify-center items-center">
+                <span>
+                  <HiOutlineComputerDesktop />
+                </span>
+              </div>
+              <div className="frontend">
+                <FrontEndSkills />
+              </div>
+            </div>
+            <div className="flex flex-col items-center bg-white bg-opacity-20 p-4 md:p-8  shadow-lg shadow-grey rounded-lg backdrop-blur-md">
+              <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10  flex justify-center items-center">
+                <span>
+                  <FaServer className="text-dark-blue" />
+                </span>
+              </div>
+              <div className="backend">
+                <BackendSkills />
+              </div>
+            </div>
+            <div className="flex flex-col items-center bg-white bg-opacity-20 p-4 md:p-8 shadow-lg shadow-grey rounded-lg backdrop-blur-md">
+              <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10 flex justify-center items-center">
+                <span>
+                  <FaRegUserCircle className="text-dark-blue" />
+                </span>
+              </div>
+              <div className="other">
+                <OtherSkills />
+              </div>
             </div>
           </div>
-          <div className="column  bg-white bg-opacity-25 p-4 md:p-8  shadow-lg shadow-grey rounded-lg backdrop-blur-md">
-          <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10  flex justify-center items-center">
-            <span ><FaServer className="text-dark-blue"/></span>
-           </div>
-            <div className="backend">
-            <BackendSkills/>
-            </div>
-          </div>
-          <div className="column bg-white bg-opacity-10 p-4 md:p-8 shadow-lg shadow-grey rounded-lg backdrop-blur-md">
-          <div className="bg-light-blue bg-opacity-25 rounded-full w-10 h-10 flex justify-center items-center">
-            <span ><FaServer className="text-dark-blue"/></span>
-           </div>
-            <div className="other">
-           
-            </div>
-          </div>
-        </div>
         </div>
       </div>
     </>
