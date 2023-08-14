6972572bf0034542473965ae94948e35186046d7
cjsolis <cjelizondo12@gmail.com>
2023-08-14 15:29:26 -0500

    added icons for navbar

--- src/components/navbar/NavMobile.jsx
+++ src/components/navbar/NavMobile.jsx
@@ -1,12 +1,28 @@
+import React, {useState} from 'react';
 import {RxHamburgerMenu} from "react-icons/rx"
+ import {TfiClose} from 'react-icons/tfi'
 
 export default function NavMobile() {
-   
+   const [nav, setNav] = useState(false)
+
+   const handleNav = () => {
+    setNav(!nav)
+   }
 
     return (
         <>
-            <div className="navbar relative px-4 flex justify-between items-center" >
-                <RxHamburgerMenu size={25}/>
+            <div className="navbar" >
+                {!nav ? <TfiClose size={20}/> : <RxHamburgerMenu size={20}/>  }
+                
+            </div>
+            <div className="fixed left-0 top-0 w-[50%] h-full bg-stone-900">
+            <h1 className="w-full text-xl text-lilac m-4">CLAUDIA SOLIS</h1>
+            <ul className="mobile-links uppercase p-4" >
+                <li className="p-2 border-b border-gray-600">About</li>
+                <li className="p-2 border-b border-gray-600">Work</li>
+                <li className="p-2 border-b border-gray-600">Skills</li>
+                <li className="p-2 border-b border-gray-600">Contact</li>
+            </ul>
             </div>
         </>
     );
