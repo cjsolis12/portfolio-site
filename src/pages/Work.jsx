5a4355ffa66f4e64d2944fb43411daad604b9492
cjsolis <cjelizondo12@gmail.com>
2023-08-15 18:47:05 -0500

    styled title under work grid

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -4,16 +4,16 @@ import { FaGithub } from "react-icons/fa";
 export default function Work() {
   return (
     <>
-      <div className="mt-64 m-10">
+      <div className="mt-64 m-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {projects.map((item, index) => (
-            <div key={index} className="flex items-center">
+            <div key={index} className="flex flex-col">
               <img
                 src={item.img}
                 alt={item.title}
                 className="object-cover w-full h-full"
               />
-              {/* <h2 className="text-lg text-lilac">{item.title}</h2> */}
+              <h2 className="text-3xl text-white mt-3">{item.title}</h2>
             </div>
           ))}
         </div>
