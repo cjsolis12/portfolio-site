1f819834722e4c1315b15b9af68c99eb20860981
cjsolis <cjelizondo12@gmail.com>
2023-08-15 18:42:46 -0500

    added grid style to main layout

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -5,24 +5,24 @@ export default function NavLinks() {
     <>
       <ul className="navigation-links hidden md:flex">
         <li className="p-4 hover:underline-slow">
-          <Link to="/about" className="hover:text-oliveGreen">
+          <Link to="/about" className="hover:text-lilac">
             About
           </Link>
         </li>
         <li className="p-4">
-          <Link to="/work" className="hover:text-oliveGreen">
+          <Link to="/work" className="hover:text-lilac">
             Work
           </Link>
         </li>
         <li className="p-4">
           {" "}
-          <Link to="/skills" className="hover:text-oliveGreen">
+          <Link to="/skills" className="hover:text-lilac">
             Skills
           </Link>
         </li>
         <li className="p-4">
           {" "}
-          <Link to="/contact" className="hover:text-oliveGreen">
+          <Link to="/contact" className="hover:text-lilac">
             Contact
           </Link>
         </li>
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -4,12 +4,16 @@ import { FaGithub } from "react-icons/fa";
 export default function Work() {
   return (
     <>
-      <div className="m-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="mt-64 m-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {projects.map((item, index) => (
-            <div key={index} className="flex flex-col items-center w-60 h-64">
-              <img src={item.img} alt={item.title} className=" object-cover" />
-              <h2 className="text-lg mt-2 text-lilac">{item.title}</h2>
+            <div key={index} className="flex items-center">
+              <img
+                src={item.img}
+                alt={item.title}
+                className="object-cover w-full h-full"
+              />
+              {/* <h2 className="text-lg text-lilac">{item.title}</h2> */}
             </div>
           ))}
         </div>
