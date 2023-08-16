b6613d95c3cc3a32bd0cb9ed333afda537c83d33
cjsolis <cjelizondo12@gmail.com>
2023-08-15 19:27:16 -0500

    icon size and links to icons

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import projects from "../data/projects";
 import { FaGithub } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 export default function Work() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -33,18 +34,19 @@ export default function Work() {
                   hoveredIndex === index ? { opacity: 0.6 } : { opacity: 1 }
                 }
                 transition={{ duration: 0.5, delay: 0.3 }}
-                
               />
-               <h2 className="text-2xl text-white mt-4">{item.title}</h2>
+              <h2 className="text-2xl text-white mt-4">{item.title}</h2>
               <motion.div
                 className={`overlay ${hoveredIndex === index ? "active" : ""}`}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: hoveredIndex === index ? 1 : 0 }}
                 transition={{ duration: 0.5 }}
               >
-                <div className=" overlay-content absolute inset-0 flex items-center justify-center text-center bg-black bg-opacity-75 p-4">
-                  <p className="text-white text-lg md:text-xl lg:text-2xl xl:text-3xl p-4">{item.description}</p>
-                  <div className="mt-4">
+                <div className=" overlay-content absolute inset-0 flex flex-col items-center justify-center text-center bg-black bg-opacity-75 p-4">
+                  <p className="text-white text-lg md:text-xl lg:text-2xl xl:text-3xl p-4">
+                    {item.description}
+                  </p>
+                  <div className="mt-4 flex text-2xl">
                     <a
                       href={item.githubLink}
                       target="_blank"
@@ -54,12 +56,12 @@ export default function Work() {
                       <FaGithub />
                     </a>
                     <a
-                      href={item.webLink}
+                      href={item.websiteLink}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-lilac"
                     >
-                      Web
+                      <FaLink />
                     </a>
                   </div>
                 </div>
