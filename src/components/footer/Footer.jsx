91054c58f3e86f2ecaeea63f822c1ff08271693f
cjsolis <cjelizondo12@gmail.com>
2023-08-20 14:14:15 -0500

    modifed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,8 @@ import { TfiEmail } from "react-icons/tfi";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="pt-4 px-2 sm:px-6 lg:px-8 text-lilac ">
+    <div className="footer p-4">
+      <div className="pt-4 px-2 sm:px-6 lg:px-8  text-lilac ">
         <div className="flex justify-items-center justify-center mt-8 space-x-6 text-2xl">
           <a
             href="https://www.linkedin.com/in/claudia-solis-48599422a/"
@@ -33,7 +33,7 @@ export default function Footer() {
           </a>
         </div>
 
-        <p className="text-center mt-4 text-gray-400">
+        <p className="text-center mt-4  text-gray-400">
           © 2023 Claudia Solis, All rights reserved.
         </p>
       </div>
