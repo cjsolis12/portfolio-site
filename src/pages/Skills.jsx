366830b86d6ba03658bdbe5140c6287b0d524819
cjsolis <cjelizondo12@gmail.com>
2023-08-20 13:17:31 -0500

    removed ul tags

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,14 +1,14 @@
 export default function Skills() {
   return (
-    <div className="mt-8">
+    <div className="m-4">
       <div className="flex justify-between gap-4 p-4 text-white">
-        <div className="w-1/4 text-5xl">
+        <div className="w-1/4 text-5xl text-white">
           Technologies.
         </div>
         <div className="flex w-3/4 gap-8">
-        <div className="flex flex-col gap-4 justify-between">
+        <div className="flex flex-col gap-4 justify-between pl-4">
           <div className="text-xl  text-lilac">FRONT-END</div>
-          <ul className="">
+         
             <li>HTML</li>
             <li>CSS</li>
             <li>Javascript</li>
@@ -17,28 +17,27 @@ export default function Skills() {
             <li>Tailwind</li>
             <li>BootStrap</li>
             <li>Material UI</li>
-          </ul>
+        
         </div>
         <div className="flex flex-col gap-4 ">
           <div className="text-xl  text-lilac">BACK-END</div>
-          <ul className="">
+         
             <li>Node JS</li>
             <li>Express JS</li>
             <li>GraphQL</li>
             <li>MongoDB</li>
             <li>MySQL</li>
-          </ul>
+        
         </div>
         <div className="flex flex-col gap-4 ">
           <div className="text-xl  text-lilac">Other</div>
-          <ul className="">
+       
             <li>Git</li>
             <li>GitHub</li>
+           
             <div className="text-xl  text-lilac">Cloud</div>
             <li>AWS</li>
             <li>FireStore</li>
-
-          </ul>
         </div>
         </div>
       </div>
