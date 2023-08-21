bd0a1d1e015fc2503956d6e79e1d4aebc0fc9761
cjsolis <cjelizondo12@gmail.com>
2023-08-21 11:44:29 -0500

    created extrawork data.js to loop over for cards

--- src/components/work/ExtraWork.jsx
+++ src/components/work/ExtraWork.jsx
@@ -1,3 +1,5 @@
+import extraWork from "../../data/extraWork";
+
 export default function ExtraWork() {
   return (
     <>
@@ -6,54 +8,22 @@ export default function ExtraWork() {
           Other Projects / Work
         </h2>
         <div className="gap-8 md:flex justify-center p-8">
-          <div className="other-projects  text-white">
-            <h3 className="text-center m-4 text-2xl">Case Studies</h3>
-            <p>
-              This project involved conducting interviews with our top clients
-              to highlight how Documint has been beneficial to their work. I
-              compiled the interview findings for publications on Documint's
-              company website.
-            </p>
-            <a
-              href="https://documint.me/case-study"
-              className="btn-link text-lilac"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit Site
-            </a>
-          </div>
-
-          <div className="other-projects  text-white">
-            <h3 className="text-center m-4 text-2xl">Documentation</h3>
-            <p>
-              Through collaboration, I gained a deep appreciation for the significance of implementing efficient technical documentation. This assignment provided an opportunity to leverage my teaching skills while simultaneously acquiring a comprehensive understanding of technical documentation.
-            </p>
-            <a
-              href="https://docs.documint.me/"
-              className="btn-link text-lilac"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit Site
-            </a>
-          </div>
-          <div className="other-projects  text-white">
-            <h3 className="text-center m-4 text-2xl">Password Generator</h3>
-            <p>
-              Instantly generate a secure, random password with this tool. This
-              app features dynamically updated HTML and CSS powered by
-              Javascript code.
-            </p>
-            <a
-              href="https://cjsolis12.github.io/Password_Generator/"
-              className="btn-link text-lilac"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit Site
-            </a>
-          </div>
+          {projects.map((project, index) => (
+            <div key={index} className="other-projects text-white">
+              <h3 className="text-center m-4 text-2xl">{project.title}</h3>
+              <p>{project.description}</p>
+              <div className="mt-auto">
+                <a
+                  href={project.link}
+                  className="btn-link text-lilac"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Visit Site
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
---
+++ src/data/extraWork.js
@@ -0,0 +1,20 @@
+const extraWork = [
+    {
+      title: "Case Studies",
+      description:
+        "This project involved conducting interviews with our top clients to highlight how Documint has been beneficial to their work. I compiled the interview findings for publications on Documint's company website.",
+      link: "https://documint.me/case-study",
+    },
+    {
+      title: "Documentation",
+      description:
+        "This assignment provided an opportunity to leverage my teaching skills while simultaneously acquiring a comprehensive understanding of technical documentation. I restructured the layout for the pages and added documentation for new features.",
+      link: "https://docs.documint.me/",
+    },
+    {
+      title: "Password Generator",
+      description:
+        "Instantly generate a secure, random password with this tool. This app features dynamically updated HTML and CSS powered by Javascript code.",
+      link: "https://cjsolis12.github.io/Password_Generator/",
+    },
+  ];
