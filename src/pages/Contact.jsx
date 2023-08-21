3e04bfc6eb4ceaf512d1b5e2a1674123c2e4365d
cjsolis <cjelizondo12@gmail.com>
2023-08-20 19:31:51 -0500

    fixed width and inputs of contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,69 +1,57 @@
 export default function Contact() {
   return (
     <>
-      <div className="w-full md:w-96 p-2 md: mx-50">
-        <div className="flex flex-col p-8 border border-lilac">
-          <form method="POST" action="">
-            <label className="block mb-6">
-              <span className="text-gray-700">Your name</span>
+      <div className="flex justify-center items-center py-12 ">
+        <div className=" bg-white border rounded-lg shadow-lg p-8 w-1/3">
+        <h2 className="text-2xl font-bold text-lilac">Send an Email!</h2>
+        <div className="mt-8 w-1/3">
+          <div className="grid grid-cols-1 gap-6">
+            <label className="block mt-4">
+              <span className="text-lilac">Full name</span>
               <input
                 type="text"
-                name="name"
                 className="
-            block
-            w-full
-            mt-1
-            border-gray-300
-            rounded-md
-            shadow-sm
-            focus:border-indigo-300
-            focus:ring
-            focus:ring-indigo-200
-            focus:ring-opacity-50
-          "
-                placeholder="Name"
+                    mt-1
+                    w-full
+                    block
+                    rounded
+                    border-gray-300
+             
+                    focus:border-primary focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  "
+                placeholder=""
               />
             </label>
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Email
-              </span>
+            <label className="mt-4">
+              <span className="text-lilac">Email address</span>
               <input
                 type="email"
-                name="email"
-                class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                placeholder="you@example.com"
+                className="
+                    mt-1
+                    block
+                    w-full
+                    border-gray-300
+                    focus:border-lilac focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  "
+                placeholder="john@example.com"
               />
             </label>
-            <label className="block mb-6">
-              <span className="text-gray-700">Message</span>
+            <label className="mt-4">
+              <span className="text-lilac">Message</span>
               <textarea
-                name="message"
                 className="
-            block
-            w-full
-            mt-1
-            border-gray-300
-            rounded-md
-            shadow-sm
-            focus:border-indigo-300
-            focus:ring
-            focus:ring-indigo-200
-            focus:ring-opacity-50
-          "
+                    mt-1
+                    block
+                    border-gray-300
+                    w-full
+                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  "
                 rows="3"
-                placeholder="Send me a message!"
               ></textarea>
             </label>
-            <div className="mb-6">
-              <button
-                type="submit"
-                className="btn bg-lilac text-charcoal w-3 h-2"
-              >
-                Contact Me
-              </button>
-            </div>
-          </form>
+            <button className="mt-4">Send</button>
+          </div>
+        </div>
         </div>
       </div>
     </>
