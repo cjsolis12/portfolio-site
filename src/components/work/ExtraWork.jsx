982b617a28c90921bcbdb652dbfc9a4df64e27b2
cjsolis <cjelizondo12@gmail.com>
2023-08-21 11:14:00 -0500

    adding other projects and styling

--- src/components/work/ExtraWork.jsx
+++ src/components/work/ExtraWork.jsx
@@ -5,15 +5,18 @@ export default function ExtraWork() {
         <h2 className="p-8  text-lilac text-5xl text-center ">
           Other Projects / Work
         </h2>
-        <div className="flex gap-8 justify-center">
-          <div className="other-projects  text-white">
-            <h3 className="text-center m-4">Case Studies</h3>
+        <div className="flex gap-8 justify-center p-8">
+          <div className="other-projects flex-grow text-white">
+            <h3 className="text-center m-4 text-2xl">Case Studies</h3>
+            <p>This project involved conducting interviews with our top clients to highlight how Documint has been beneficial to their work. I compiled the interviews findings for publications on Documint's company website.</p>
+          </div>
+          <div className="other-projects flex-grow text-white">
+            <h3 className="text-center m-4 text-2xl">Password Generator</h3>
+            <p>Instantly generate a secure, random password with this tool. This app features dynamically updated HTML and CSS powered by Javascript code.</p>
+            <link rel="link" href="https://cjsolis12.github.io/Password_Generator/"/>
           </div>
           <div className="other-projects  text-white">
-            <h3 className="text-center m-4"></h3>
-          </div>
-          <div className="other-projects  text-white">
-            <h3 className="text-center m-4">Case Studies</h3>
+            <h3 className="text-center m-4 text-2xl">Case Studies</h3>
           </div>
         </div>
       </div>
