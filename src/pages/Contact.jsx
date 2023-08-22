59778d8eb1b746895deed83eb9053d3ab000e578
cjsolis <cjelizondo12@gmail.com>
2023-08-22 08:22:17 -0500

    added emailjs to contact form and messages sent work

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,54 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+
 export default function Contact() {
+    //Email Functions
+    const form = useRef();
+
+    // Function to show toast notification
+    const notifySuccess = () => {
+      toast.success("Email sent successfully!", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 2000,
+      });
+    };
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+      const user_email = e.target.elements.user_email.value;
+  
+      emailjs
+        .sendForm(
+          "service_b49i52f",
+          "template_dbj0gog",
+          form.current,
+          "g3ShUZPhcjLjaPsm_"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            notifySuccess();
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    };
   return (
     <>
       <div className="center-screen">
         <div className="contact-form bg-lilac rounded-lg p-8 ">
         <h2 className="text-3xl font-bold text-lilac text-center">Send an Email!</h2>
+        <form ref={form} onSubmit={sendEmail}>
         <div className=" mt-8  text-1xl">
           <div className="flex flex-col gap-6 m-4">
             <label className="block mt-4 mb-4">
               <span className="text-lilac">Full name</span>
               <input
                 type="text"
+                required
+                name="from_name"
                 className="
                     mt-1
                     w-full
@@ -25,6 +64,7 @@ export default function Contact() {
             <label className="mt-4 pb-4">
               <span className="text-lilac">Email address</span>
               <input
+              name="user_email"
                 type="email"
                 className="
                     mt-1
@@ -39,6 +79,8 @@ export default function Contact() {
             <label className="mt-4">
               <span className="text-lilac">Message</span>
               <textarea
+              name="message"
+              type="text"
                 className="
                     mt-1
                     block
@@ -50,11 +92,14 @@ export default function Contact() {
               ></textarea>
             </label>
             <div>
-            <button className="send-btn mt-4">Send</button>
+            <button className="send-btn mt-4" type="submit" >Send</button>
             </div>
           </div>
         </div>
+        </form>
+     
         </div>
+        <ToastContainer></ToastContainer>
       </div>
     </>
   );
