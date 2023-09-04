6157bdf3c4d9e40c3fcc8e104f09df22d9957089
cjsolis <cjelizondo12@gmail.com>
2023-09-04 11:43:33 -0500

    fixed the width of the contact form on larger screens

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,51 +2,51 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Contact() {
-    //Email Functions
-    const form = useRef();
+  //Email Functions
+  const form = useRef();
 
-    // Function to show toast notification
-    const notifySuccess = () => {
-      toast.success("Email sent successfully!", {
-        position: toast.POSITION.BOTTOM_CENTER,
-        autoClose: 2000,
-      });
-    };
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-      
-      emailjs
-        .sendForm(
-          "service_b49i52f",
-          "template_dbj0gog",
-          form.current,
-          "g3ShUZPhcjLjaPsm_"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-            notifySuccess();
-            resetForm();
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    };
+  // Function to show toast notification
+  const notifySuccess = () => {
+    toast.success("Email sent successfully!", {
+      position: toast.POSITION.BOTTOM_CENTER,
+      autoClose: 2000,
+    });
+  };
 
-    //Function to reset form
-    const resetForm = () => {
-      form.current.reset()
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_b49i52f",
+        "template_dbj0gog",
+        form.current,
+        "g3ShUZPhcjLjaPsm_"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          notifySuccess();
+          resetForm();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  //Function to reset form
+  const resetForm = () => {
+    form.current.reset();
+  };
 
   return (
     <>
       <div className="center-screen">
-        <motion.div 
+        <motion.div
           variants={{
             hidden: { opacity: 0, y: 200 },
             visible: { opacity: 1, y: 0 },
@@ -54,19 +54,21 @@ export default function Contact() {
           initial="hidden"
           animate="visible"
           transition={{ duration: 0.5, delay: 0.25 }}
-        
-        className="contact-form bg-lilac rounded-lg p-8 ">
-        <h2 className="text-3xl font-bold text-lilac text-center">Send an Email!</h2>
-        <form ref={form} onSubmit={sendEmail}>
-        <div className=" mt-8  text-1xl">
-          <div className="flex flex-col gap-6 m-4">
-            <label className="block mt-4 mb-4">
-              <span className="text-lilac">Full name</span>
-              <input
-                type="text"
-                required
-                name="from_name"
-                className="
+          className="contact-form-container bg-lilac rounded-lg p-8 "
+        >
+          <h2 className="text-3xl font-bold text-lilac text-center">
+            Send an Email!
+          </h2>
+          <form ref={form} onSubmit={sendEmail}>
+            <div className=" mt-8  text-1xl">
+              <div className="flex flex-col gap-6 m-4">
+                <label className="block mt-4 mb-4">
+                  <span className="text-lilac">Full name</span>
+                  <input
+                    type="text"
+                    required
+                    name="from_name"
+                    className="
                     mt-1
                     w-full
                     block
@@ -75,46 +77,47 @@ export default function Contact() {
              
                     focus:border-primary focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                   "
-                placeholder="John Doe"
-              />
-            </label>
-            <label className="mt-4 pb-4">
-              <span className="text-lilac">Email address</span>
-              <input
-              name="user_email"
-                type="email"
-                className="
+                    placeholder="John Doe"
+                  />
+                </label>
+                <label className="mt-4 pb-4">
+                  <span className="text-lilac">Email address</span>
+                  <input
+                    name="user_email"
+                    type="email"
+                    className="
                     mt-1
                     block
                     w-full
                     border-gray-300
                     focus:border-lilac focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                   "
-                placeholder="john@example.com"
-              />
-            </label>
-            <label className="mt-4">
-              <span className="text-lilac">Message</span>
-              <textarea
-              name="message"
-              type="text"
-                className="
+                    placeholder="john@example.com"
+                  />
+                </label>
+                <label className="mt-4">
+                  <span className="text-lilac">Message</span>
+                  <textarea
+                    name="message"
+                    type="text"
+                    className="
                     mt-1
                     block
                     border-gray-300
                     w-full
                     focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                   "
-                rows="3"
-              ></textarea>
-            </label>
-            <div>
-            <button className="send-btn mt-4" type="submit" >Send</button>
+                    rows="3"
+                  ></textarea>
+                </label>
+                <div>
+                  <button className="send-btn mt-4" type="submit">
+                    Send
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        </form>
-     
+          </form>
         </motion.div>
       </div>
       <ToastContainer></ToastContainer>
